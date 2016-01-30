7ed2185dbe13459d040ddb63f6c49abd15ff98b3
Nicole Grimwood <nicolegrimwood@gmail.com>
2016-01-29 21:51:14 -0500

    added some comments for structure

--- reverb.py
+++ reverb.py
@@ -1,2 +1,14 @@
 # Reverb
+# Using a series of all pass filters
+
+# read some audio file (real-time tbd)
+
+# determine greatest delay value and store that number of values
+# most efficent way to remove an item from one side of an array and add it to the other?
+
+
+# Sample at some reasonable rate
+# compute all pass filtered components (assuming signal has lasted long enough) and add
+# play current audio
+
 
