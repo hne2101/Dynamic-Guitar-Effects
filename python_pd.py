a0744c300da0c7bf9f2b3a08d98e1a6af3b10e18
Nicole Grimwood <nicolegrimwood@gmail.com>
2016-03-08 18:14:26 -0500

    Successfully send message to pd from python

--- python_pd.py
+++ python_pd.py
@@ -1,11 +1,15 @@
 # Python and PD interaction script
+from socket import *
 import os, sys
 
 def volume(message=''):
 	os.system("1 75 | pdsend 3000")
 
 def dspOn():
-	os.system("echo '0' | pdsend 3000")
+	s = socket(AF_INET,SOCK_STREAM)
+	s.connect(('localhost',3000))
+	s.send('10;\n')
+	s.close()
 
 if __name__ == "__main__":
 	dspOn()
